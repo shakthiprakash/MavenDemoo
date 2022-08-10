a915e844ea0ed475b89b4cbc247e71ad6c724ecb
Shakthi <shakthiprakash2001@gmail.com>
2022-08-10 14:17:21 +0530

    hey

---
+++ MavenDemo2/src/test/java/com/lti/Demo/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.lti.Demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+     
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
